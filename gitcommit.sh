57d0d72fcebfdfaf113c2b57b4492e8a28477c2e
kneerunjun <kneerunjun@gmail.com>
2017-09-06 23:54:31 +0000

    this is a sample commit

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+read -p "What commit message would you like ? "  msg
 git add .
-git commit -m "sample commit from the script"
+git commit -m "$msg"
 git push http://kneerunjun:kneeru2git@github.com/PiFarm/co2sensing.git master
