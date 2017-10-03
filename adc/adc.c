a26b46963f583075530c113f160f30b6b8f7b2bf
kneerunjun <kneerunjun@gmail.com>
2017-10-03 02:27:47 +0000

    working base library of adc is now ready for use

--- adc/adc.c
+++ adc/adc.c
@@ -20,52 +20,80 @@ config[2]     : lsb of the configuration
 slaveaddr     : depending on the hardware configuraion you can actually set this, default 0x48
 error         : this is the success indicator < 0 : Error  ==0 is Success , -1 == device error , -2 : register error
 */
-int ads115_read_volts(int slaveaddr,float* readings){
-  const float VPS = 4.096/32767.0 ;// since we intend to keep the gain at 4.096V
-  const int slave_addr =slaveaddr;
-  uint8_t a1Config[3];
-  a1Config[0]=1;
-  a1Config[1]=0b11010011;
-  a1Config[2]=0b10000011;
+
+float read_device(uint8_t conf[], int addr ,int* error){
+  int fd; //this is the device pointer.
   int sps=128;
-  int fd; //this is the open file pointer.
+  const float VPS = 4.096/32767.0 ;// since we intend to keep the gain at 4.096V
   int16_t val;
   uint8_t readBuffer[3] ;
   if ((fd = open("/dev/i2c-1", O_RDWR)) <0) {
     printf("Could not open device %d\n",fd );
     return -1;
   }
-  if(ioctl(fd, I2C_SLAVE, slave_addr) < 0){
+  if(ioctl(fd, I2C_SLAVE, addr) < 0){
     printf("Failed to connect to I2C slave\n");
     return -1;
   }
-  if (write(fd, a1Config, 3)!=3) {
+  if (write(fd, conf, 3)!=3) {
     perror("configuration write error:");
     printf("There was error writing to the configuration register\n");
-    return -1;
+    *error= -1;
+    return 0;
   }
   do {
-    if (read(fd, a1Config, 2)!=2) {
+    if (read(fd, conf, 2)!=2) {
       printf("Could not read the register \n" );
-      return -1;
+      *error= -1;
+      return 0;
     }
-  } while(a1Config[0] & 0x80 ==0);
+  } while(conf[0] & 0x80 ==0);
   usleep((1/(float)sps)*1000000+2000);
   readBuffer[0] = 0;
   if (write(fd, readBuffer,1)!=1) {
-    return -2;
+    *error= -2;
+    return 0;
   }
   // and then we go ahead to read from the conversion register
   if (read(fd, readBuffer, 2)!=2) {
     printf("Error reading the conversion register\n");
-    return -2;
+    *error= -2;
+    return 0;
   }
   val  = (readBuffer[0] <<8 | readBuffer[1]);
   if (val <0) {
     val =0.00;
   }
   close(fd);
-  *(readings) = 0.00;
-  *(readings+1) =  val * VPS;
+  return val*VPS;
+}
+int ads115_read_volts(int slaveaddr,float* readings){
+  const int slave_addr =slaveaddr;
+  uint8_t a1Config[3],a0Config[3], a2Config[3];
+  a0Config[0]=1;
+  a0Config[1]=0b11000011;
+  a0Config[2]=0b10000011;
+  a1Config[0]=1;
+  a1Config[1]=0b11010011;
+  a1Config[2]=0b10000011;
+  a2Config[0]=1;
+  a2Config[1]=0b11100011;
+  a2Config[2]=0b10000011;
+  int err =0;
+  float  volts  = read_device(a0Config, 0x48,&err);
+  // printf("%.5f\n", volts);
+  if (err==0) {
+    *(readings) = volts;
+  }
+  volts  = read_device(a1Config, 0x48,&err);
+  // printf("%.5f\n", volts);
+  if (err==0) {
+    *(readings+1) = volts;
+  }
+  volts  = read_device(a2Config, 0x48,&err);
+  // printf("%.5f\n", volts);
+  if (err==0) {
+    *(readings+2) = volts;
+  }
   return 0;
 }
--- adc/adctest.c
+++ adc/adctest.c
@@ -26,10 +26,12 @@ int main(int argc, char const *argv[]) {
   // float a2  =ads115_read_volts(writeBuffer, 0x48, &error);
   // printf("%.3f\t\t%.3f\t\t%.3f", a0,a1,a2);
   float voltages[4];
+  printf("A0(Volts)\t\tA1(Volts)\t\tA2(Volts)\n");
+  printf("=======================================\n");
   while(1){
     float voltages[4];
     int result  =ads115_read_volts(0x48,voltages);
-    printf("%.4f\n", voltages[1]);
+    printf("%.4f\t\t%.4f\t\t%.4f", *voltages, *(voltages+1),*(voltages+2));
     sleep(3);
   }
   return 0;
