c6effea69fd65509256e669eea36993b8ead9395
kneerunjun <kneerunjun@gmail.com>
2017-10-05 23:52:41 +0000

    all the channels are now workingfine for #7

--- adc/adctest.c
+++ adc/adctest.c
@@ -3,28 +3,31 @@
 #include<unistd.h>
 #include <wiringPi.h>
 #include "adc.h"
-
+void test_singlechannel_read(){
+  int ok =0;
+  PGA gain = GAIN_EIGHT;
+  DTRATE dr = DR_128;
+  float voltsA1=ads115_read_channel(0x48, 1,gain, dr,&ok);
+  gain = GAIN_TWO;// we know from direct measurement LDR voltage is around 1.24 V
+  float voltsA2 =ads115_read_channel(0x48, 2,gain, dr,&ok);
+  if (ok==0) {
+    printf("%.4f\t\t%.4f\n",voltsA1, voltsA2 );
+  }
+}
+void test_allchannels_read(){
+  float voltages[4]; //all the channels' readings
+  int result =ads115_read_all_channels(0x48, voltages);
+  if (result==0) {
+    printf("%.4f\t\t%.4f\t\t%.4f\t\t%.4f\n", *voltages,  *(voltages+1), *(voltages+2),*(voltages+3));
+  }
+}
 int main(int argc, char const *argv[]) {
   wiringPiSetupGpio();
-  uint8_t writeBuffer[3] ;
   int error =0;
   printf("Starting conversions ..\n");
-  float voltages[4]; //all the channels' readings
   while(1){
-    // int result =ads115_read_volts(0x48, voltages);
-    // if (result==0) {
-    //   printf("%.4f\t\t%.4f\t\t%.4f\t\t%.4f\n", *voltages,  *(voltages+1), *(voltages+2),*(voltages+3));
-    // }
-    // this is where we go ahead to test the single channel read ..
-    int ok =0;
-    PGA gain = GAIN_EIGHT;
-    DTRATE dr = DR_128;
-    float voltsA1=ads115_read_channel(0x48, 1,gain, dr,&ok);
-    gain = GAIN_TWO;// we know from direct measurement LDR voltage is around 1.24 V
-    float voltsA2 =ads115_read_channel(0x48, 2,gain, dr,&ok);
-    if (ok==0) {
-      printf("%.4f\t\t%.4f\n",voltsA1, voltsA2 );
-    }
+    test_allchannels_read();
+    // test_singlechannel_read();
     sleep(3);
   }
   return 0;
