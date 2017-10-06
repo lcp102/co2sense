9e78ca49b04e1458bee2caa5e636555a29c9a684
kneerunjun <kneerunjun@gmail.com>
2017-10-06 10:26:23 +0000

    now created a library for adc as mentioned in #7

--- adc/run.sh
+++ adc/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # this is to build and run the adctest
 gcc -c adc.c -I .
+ar crv libadc.a adc.o
+ranlib libadc.a
 gcc -c adctest.c -I .
-gcc -o adctest adctest.o adc.o -lwiringPi
+gcc -o adctest adctest.o -L. -ladc -lwiringPi
 ./adctest
