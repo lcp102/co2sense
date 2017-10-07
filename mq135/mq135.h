7db6d64516ed7dbee02b8300899756d022c9c71a
kneerunjun <kneerunjun@gmail.com>
2017-10-07 09:07:43 +0000

    mq135 has been modularised into a library

--- mq135/mq135.h
+++ mq135/mq135.h
@@ -5,7 +5,7 @@
 #define CO2_SLOPE -0.370955166
 //this is the load resistance on the sensor, you would perhaps have to measure on the device
 #define LOAD_RESISTANCE_KOHMS 2.5
-#define CO2_PPM_NOW 405.07 // refer https://www.co2.earth/
+#define CO2_PPM_NOW 402.73 // refer https://www.co2.earth/
 #define VDD 5.00 //this is assumed that we are connecting the sensor to this much of volts
 float ppm_co2(int* ok, int adschn, int fcalibrate);
 #endif
