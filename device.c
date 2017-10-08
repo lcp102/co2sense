543c6744f75967451cc46f1566ec2fbb9a57e60d
kneerunjun <kneerunjun@gmail.com>
2017-10-08 11:40:47 +0000

    added modularisation for lcd inline with #7

--- device.c
+++ device.c
@@ -16,6 +16,7 @@ run           : ./bin/i2ctest
 #include "./ldr/ldr.h"
 #include "./lm35/lm35.h"
 #include "./alerts/alerts.h"
+#include "./display/display.h"
 
 #define RS 9
 #define E 11
@@ -56,21 +57,21 @@ void on_interrupt(int signal){
   lcdClear(lcd);
   exit(0);
 }
-void display_marquee(float temp, float light, float co2){
-  char tempMessage[6], lightMessage[10], co2Message[16];
-  sprintf(tempMessage, "%.2f", temp);
-  sprintf(lightMessage, ":%.1f%%", light);
-  sprintf(co2Message,":%.1f",co2);
-  lcdClear(lcd);
-  lcdPuts(lcd, tempMessage);
-  lcdPutchar(lcd, 0); //0th character is defined as the deg ceclius symbol
-  lcdPuts(lcd,"  ");
-  lcdPutchar(lcd,1);
-  lcdPuts(lcd, lightMessage);
-  lcdPosition(lcd,0,1);
-  lcdPutchar(lcd,2);
-  lcdPuts(lcd, co2Message);
-}
+// void display_marquee(float temp, float light, float co2){
+//   char tempMessage[6], lightMessage[10], co2Message[16];
+//   sprintf(tempMessage, "%.2f", temp);
+//   sprintf(lightMessage, ":%.1f%%", light);
+//   sprintf(co2Message,":%.1f",co2);
+//   lcdClear(lcd);
+//   lcdPuts(lcd, tempMessage);
+//   lcdPutchar(lcd, 0); //0th character is defined as the deg ceclius symbol
+//   lcdPuts(lcd,"  ");
+//   lcdPutchar(lcd,1);
+//   lcdPuts(lcd, lightMessage);
+//   lcdPosition(lcd,0,1);
+//   lcdPutchar(lcd,2);
+//   lcdPuts(lcd, co2Message);
+// }
 int main(int argc, char const *argv[]) {
   int ok =0;
   // register a signal
@@ -79,29 +80,16 @@ int main(int argc, char const *argv[]) {
   signal(SIGTERM, &on_interrupt);
   // this is being setup from the upstart service , re setting here woudl lead to some problem
   wiringPiSetupGpio();
-  lcd = lcdInit (2,16,4,RS,E,D0,D1,D2,D3,0,0,0,0);
-  unsigned char char_degcelcius[8] = {0x18,0x18,0x3,0x4,0x4,0x4,0x3,0x0}; //defining a new symbol
-  unsigned char char_lightlambda[8]={0x4,0xa,0xa,0xa,0x11,0x1f,0xe,0x0};
-  unsigned char char_co2[8]={0x2,0x4,0xe,0x10,0x10,0x11,0xe,0x0};
-  lcdCharDef (lcd, 0, char_degcelcius) ;
-  lcdCharDef (lcd, 1, char_lightlambda) ;
-  lcdCharDef (lcd, 2, char_co2) ;
-  lcdPuts(lcd, "Sensing...");
-
-  // setting up the alerts
+  setup_lcd_4bitmode(2,16,RS,E,D0,D1,D2,D3);
   setup_alert(BLUE_GPIO, RED_GPIO, BUZZ_GPIO);
   while (1) {
-
     float ppm=ppm_co2(&ok, 0, 0);
     if(ok!=0){perror("device.c: failed to get the co2 footprint");continue;}
-
     float temp  = airtemp_now(&ok,1, CELCIUS);
     if(ok!=0){perror("We have a problem reading the temperature channel on the ADS");}
-
     float light =light_percent(&ok, 2, BRIGHT_VOLTS, DARK_VOLTS);
     if(ok!=0){perror("We have a problem reading the light intensity");}
-
-    display_marquee(temp,light*100,ppm);
+    display_readings(temp, light*100, ppm);
     if (ppm <=700.00) {alert(&ok, 0, 0);}
     else if(ppm>700.00 && ppm<=1500.00){alert(&ok, 1, 0);}
     else if(ppm>1500.00){alert(&ok, 2, 0);}
